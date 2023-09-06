a35162ba7529db440a1c3979876d49315f524719
Portafolio <oxieloziel@gmail.com>
2023-09-05 21:27:07 -0600

    pto

---
+++ src/components/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Index = () => {
+    
+
+
+
+
+    return (
+        <div>
+            <h1>Este es mi index</h1>
+        </div>
+    );
+
+};
+
+export default Index;

86e9c5c47f292c03a48118cc6ff2c43107533390
jalmarazi <josealmarazib@gmail.com>
2023-09-12 23:55:12 -0600

    UpdateSinColores

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import React, { useState } from "react";
 import Index from "./components/index.jsx";
+import Footer from "./components/footer.jsx";
 import NavbarReact from "./components/nav.jsx";
 import Login from "./components/login.jsx";
 import Register from "./components/register.jsx";
 import Carrito from "./components/carrito.jsx"; 
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 
 function App() {
-  
-  const [ user, setUser ] = useState({
-    user:'',
-    firstN: '',
-    LastN: '',
-    email: '',
-    password: '',
-    Cnumber: '',
-  });
+  const location = useLocation();
 
-  const location = window.location.pathname;
+  // Verificar si la ruta actual es
+  const isLoginPage = location.pathname === "/login";
+  const isRegisterPage = location.pathname === "/register";
+
+  // Mostrar NavbarReact solo si no estás en la página de inicio de sesión o registro
+  const showNavbar = !isLoginPage && !isRegisterPage;
+
+  // Mostrar Footer solo si no estás en la página de inicio de sesión o registro
+  const showFooter = !isLoginPage && !isRegisterPage;
 
   return (
     <div className="App">
+      {/* Renderizar NavbarReact solo si no estás en la página de inicio de sesión */}
+      {showNavbar && <NavbarReact />}
       <Routes>
-        <Route path="/" element={ <Index/> } />
-
-        <Route path="/Carrito" element={ <Carrito/> } />
-        
-        <Route path="/login" element={ <Login user={ user } setUser={ setUser }/> }/>
-        <Route path="/register" element={ <Register user={ user } setUser={ setUser }/> }/>
+        <Route path="/" element={<Index />} />
+        <Route path="/Carrito" element={<Carrito />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </div>
 
+      {showFooter && <Footer />}
+    </div>
   );
 }
 
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,19 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+//import miImagen from "./images/banmer.jpg";
+//import Imagenlogo from "./images/register.png";
 import './Styles/index.css';
 
-export const Index = () => {
+
+function HeaderAndFooterExample() {
+
+  const [ampliarImagen1, setAmpliarImagen1] = useState(false);
+  const [ampliarImagen2, setAmpliarImagen2] = useState(false);
+
+  const toggleAmpliar1 = () => {
+    setAmpliarImagen1(!ampliarImagen1);
+  };
+
+  const toggleAmpliar2 = () => {
+    setAmpliarImagen2(!ampliarImagen2);
+  };
+
+  return (
+<div>
+    <div className='baner'>
+        <h2 className='subt'>
+          Welcome
+        </h2>
+    </div>
+
+<div>
+  <ul className="opciones">
+    <li>Bodas</li>
+    <li>Paquetes</li>
+    <li>Ofertas</li>
+    <li>Restaurantes</li>
+  </ul>
+</div>
+
+    <div className='fondo'>
     
+     <div className="planner">
+      
+     <div id="imagen-contenedor" onClick={toggleAmpliar1}>
+     <img
+      // src={miImagen}
+       alt="Descripción de la imagen" 
+       id="imagen" 
+       className={`imagen-planner ${ampliarImagen1 ? "ampliada" : ""}`} // Aplica el estilo de zoom a la imagen
+       />
+     </div>
 
+     <h1 className="titulo-planner">Titulo-Nombre</h1>
+     <p className="descripcion">Descripcion: 
+     Bienvenidos a "Amor Eterno", un evento mágico diseñado y coordinado por nuestro experimentado equipo
+     de wedding planners. Esta es una celebración única y personalizada que marca el inicio de una vida
+     juntos llena de amor y promesas. En "Amor Eterno", creamos un entorno encantador donde los sueños se
+     hacen realidad y los corazones se unen en un día inolvidable.</p>
+     <Link to="/Carrito"><button className="button-planner">Ver más</button></Link>
+     
+     
+     </div>
 
+    <div className="planner">
 
+    <div id="imagen-contenedor" onClick={toggleAmpliar2}>
+     <img
+      // src={miImagen}
+       alt="Descripción de la imagen" 
+       id="imagen" 
+       className={`imagen-planner ${ampliarImagen2 ? "ampliada" : ""}`} // Aplica el estilo de zoom a la imagen
+       />
+     </div>
 
-    return (
-        <div>
-            <h1>Este es mi index</h1>
-        </div>
-    );
+     <h1 className="titulo-planner">Titulo-Nombre</h1>
+     <p className="descripcion">Descripcion: 
+     Bienvenidos a "Amor Eterno", un evento mágico diseñado y coordinado por nuestro experimentado equipo
+     de wedding planners. Esta es una celebración única y personalizada que marca el inicio de una vida
+     juntos llena de amor y promesas. En "Amor Eterno", creamos un entorno encantador donde los sueños se
+     hacen realidad y los corazones se unen en un día inolvidable.</p>
+     <Link to="/Carrito"><button className="button-planner">Ver más</button></Link>
+     
+     </div>
 
-};
+    </div>
 
-export default Index;
+</div>
+    
+  );
+}
+
+export default HeaderAndFooterExample;

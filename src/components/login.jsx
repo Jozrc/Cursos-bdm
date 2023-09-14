8f327733e645dcfed191cb753c0edd2db1a927dc
jalmarazi <josealmarazib@gmail.com>
2023-09-13 23:53:33 -0600

    Validaciones Login y Register

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,39 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import './Styles/login.css';
 import miImagen from "./images/LoginBDM.jpeg";
 
 const Login = () => {
-    return (
-      <div className="container">
+  // Define estados para almacenar los valores de los campos de entrada
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  // Función para manejar el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita la recarga de la página
+
+    // Aquí puedes realizar validaciones y procesamiento de datos
+    if (username.trim() === "") {
+      alert("Por favor, ingresa un nombre de usuario.");
+      return;
+    }
+
+    if (password.trim() === "") {
+      alert("Por favor, ingresa una contraseña.");
+      return;
+    }
+
+    // Ahora puedes enviar los datos al servidor o realizar otras acciones necesarias
+    // Por ejemplo, podrías enviar los datos a través de una solicitud HTTP
+
+    console.log("Nombre de usuario:", username);
+    console.log("Contraseña:", password);
+
+    window.location.href = "http://localhost:3000/";
+    // Luego puedes redirigir al usuario a otra página, si es necesario
+    // Ejemplo: history.push('/dashboard');
+  };
+
+  return (
+    <div className="container">
       <div className="center-container">
-
-        <img src={miImagen} alt="Descripción de la imagen" className="imagen-izquierda"/>
-
-        <form action="" className="form_main">
+        <img src={miImagen} alt="Descripción de la imagen" className="imagen-izquierda" />
+        <form onSubmit={handleSubmit} className="form_main">
           <h1 className="heading">Login</h1>
-
           <h5 className="user-passw">Username</h5>
           <div className="inputContainer">
-            <input type="text" className="inputField" id="username" />
+            <input
+              type="text"
+              className="inputField"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
-  
           <h5 className="user-passw">Password</h5>
           <div className="inputContainer">
-            <input type="password" className="inputField" id="password" />
+            <input
+              type="password"
+              className="inputField"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <Link to="/" className="forgotLink">Forgot your password?</Link>
-  
-          <Link to="/"><button className="button">Submit</button></Link>
-          <p className="dont-account">Don´t have an account yet? </p>
+          <button className="button">Submit</button>
+          <p className="dont-account">Don't have an account yet? </p>
           <Link to="/register" className="RegisterLink">Create an account</Link>
-
         </form>
       </div>
+    </div>
+  );
+};
 
-      </div>
-    );
-  };
-  
-  export default Login;
+export default Login;
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,20 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import './Styles/login.css';
 import miImagen from "./images/LoginBDM.jpeg";
 import miImagenR from "./images/registerBDM.png";
 
 
-const Register = ({user, setUser}) => {
+const Register = () => {
 
-    const handleClick = e => {
+  const [errors, setErrors] = useState({}); // Estado para almacenar errores
 
-      setUser({
-        ...user,
-        [e.target.name]: e.target.value
-      })
-      console.log(user);
+  const [user, setUser] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [pass, setPass] = useState("");
+  const [email, setEmail] = useState("");
+  const [cnumber, setCnumber] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [gender, setGender] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita la recarga de la página
+
+    //REVISAR QUE NO ESTEN VACIOS
+
+    if (firstName.trim() === "") {
+      alert("Por favor, rellena todos los campos.");
+      return;
     }
+
+    if (lastName.trim() === "") {
+      alert("Por favor, rellena todos los campos.");
+      return;
+    }
+
+    if (email.trim() === "") {
+      alert("Por favor, rellena todos los campos.");
+      return;
+    }
+
+    if (cnumber.trim() === "") {
+      alert("Por favor, rellena todos los campos.");
+      return;
+    }
+
+    if (pass.trim() === "") {
+      alert("Por favor, rellena todos los campos.");
+      return;
+    }
+
+        if (user.trim() === "") {
+      alert("Por favor, rellena todos los campos.");
+      return;
+    }
+
+  // Validación para campos "First Name" y "Last Name" (sin números)
+    if (/\d/.test(firstName)) {
+      alert("Por favor, no ingrese numeros en el campo de First Name.");
+      return;
+    }
+
+    if (/\d/.test(lastName)) {
+      alert("Por favor, no ingrese numeros en el campo de Last Name.");
+      return;
+    }
+
+     // Validación para el campo "Email"
+
+     if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+      alert("Por favor, ingrese un formato de correo electronico valido.");
+      return;
+    }
+
+    // Validacion para campo de Numero
+
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+      alert("Por favor, ingrese un formato de correo electronico valido.");
+      return;
+    }
+
+        // Validación para el campo "Password"
+
+    if (!/^\d+$/.test(cnumber)) {
+          alert("Por favor, el numero de contacto solo puede tener caracteres numericos");
+          return;
+        }
+
+    // Ahora puedes enviar los datos al servidor o realizar otras acciones necesarias
+    // Por ejemplo, podrías enviar los datos a través de una solicitud HTTP
+
+    window.location.href = "http://localhost:3000/";
+    // Luego puedes redirigir al usuario a otra página, si es necesario
+    // Ejemplo: history.push('/dashboard');
+  };
+
 
     return (
       <div className="container">
@@ -23,35 +101,48 @@ const Register = ({user, setUser}) => {
 
         <img src={miImagenR} alt="Descripción de la imagen" className="imagen-izquierdaR"/>
 
-        <form action="" className="form_main2">
+        <form action="" className="form_main2" onSubmit={handleSubmit}>
           <h1 className="heading2">Register</h1>
 
           
           <div className="user-passw-container">
           <h5 className="user-passw">First Name</h5>
           <h5 className="user-passw">Last Name</h5>
-          </div>
-          <div className="inputContainer">
-            <input type="text" name="firstN" onChange={handleClick} className="inputField2" />
-            <input type="text" name="LastN" onChange={handleClick} className="inputField2" />
-          </div>
+        </div>
+        <div className="inputContainer">
+          <input
+            type="text"
+            name="firstN"
+            onChange={(e) => setFirstName(e.target.value)}
+            className="inputField2"
+          />
+          <span className="error">{errors.firstN}</span> {/* Mostrar error */}
+          <input
+            type="text"
+            name="LastN"
+            onChange={(e) => setLastName(e.target.value)}
+            className="inputField2"
+          />
+          <span className="error">{errors.LastN}</span> {/* Mostrar error */}
+        </div>
+
           <h5 className="user-passw">User</h5>
           <div className="inputContainer">
-            <input type="text" name="user" onChange={handleClick} className="inputField" />
+            <input type="text" name="user" onChange={(e) => setUser(e.target.value)} className="inputField" />
           </div>
           <h5 className="user-passw">Email</h5>
           <div className="inputContainer">
-            <input type="email" name="email" onChange={handleClick} className="inputField" />
+            <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} className="inputField" />
           </div>
 
           <h5 className="user-passw">Password</h5>
           <div className="inputContainer">
-            <input type="password" name="password" onChange={handleClick} className="inputField" />
+            <input type="password" name="password" onChange={(e) => setPass(e.target.value)} className="inputField" />
           </div>
 
           <h5 className="user-passw">Contact Number</h5>
           <div className="inputContainer">
-            <input type="gmail" name="Cnumber" onChange={handleClick} className="inputField" />
+            <input type="gmail" name="Cnumber" onChange={(e) => setCnumber(e.target.value)} className="inputField" />
           </div>
   
           <div className="user-passw-container">
@@ -59,8 +150,8 @@ const Register = ({user, setUser}) => {
           <h5 className="user-passw">Gender</h5>
           </div>
           <div className="inputContainer">
-            <input type="date" name="birtday" onChange={handleClick} className="inputField2" />
-            <select name="rol" onChange={handleClick} className="inputField2">
+            <input type="date" name="birtday" onChange={(e) => setBirthdate(e.target.value)} className="inputField2" />
+            <select name="rol" onChange={(e) => setGender(e.target.value)} className="inputField2">
               <option value="value1">M</option>
               <option value="value2" selected>F</option>
             </select>
@@ -71,14 +162,14 @@ const Register = ({user, setUser}) => {
           </div>
 
           <div className="inputContainer">
-            <select name="rol" onChange={handleClick} className="inputField3">
+            <select name="rol" onChange={(e) => setGender(e.target.value)} className="inputField3">
               <option value="value1">Vendedor</option>
               <option value="value2" selected>Comprador</option>
               <option value="value3">Vendedor/Comprador</option>
             </select>
           </div>
 
-          <Link to="/"><button className="button">Submit</button></Link>
+          <button className="button">Submit</button>
           <p className="dont-account">Do you already have an account? </p>
           <Link to="/login" className="RegisterLink">Log in</Link>
 
